717df85a7e842105154842a6eae6eed648695ca5
WIN7-20160818PQ\Administrator <calmsbs@naver.com>
2017-04-26 10:12:29 +0900

    no message

--- myProject/src/main/java/com/soccerkick/controller/userController.java
+++ myProject/src/main/java/com/soccerkick/controller/userController.java
@@ -1,6 +1,9 @@
 package com.soccerkick.controller;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,15 +23,30 @@ public class userController {
 	
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	  public void login(Model model) throws Exception {
+	  public String login(Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		HttpSession session = request.getSession();
+		
+		if(session.getAttribute("login")!=null){  
+			return "/";  
+		} 
+		else 
+			return "redirect:/user/login";  
+		
+			
 	  }
 	
 	@RequestMapping(value = "/loginCheck", method = RequestMethod.POST)
-	  public String loginCheck(Model model, userVO vo, RedirectAttributes rttr) throws Exception {
+	  public String loginCheck(Model model, userVO vo, RedirectAttributes rttr, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		HttpSession session = request.getSession();
+		
 		int result = dao.userCheck(vo);
 		
-		if(result == 1)
+		if(result == 1){
+			session.setAttribute("login", vo);
 			return "redirect:/";
+		}
 		else
 			return "redirect:/user/login";
 		
