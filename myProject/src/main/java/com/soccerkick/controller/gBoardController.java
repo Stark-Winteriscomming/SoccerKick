c04b009d0dd74df6e70b23d9c7c32abff68fdb84
changho <chrisvan@naver.com>
2017-06-12 16:43:58 +0900

    정리, admin_member_list delete

--- myProject/src/main/java/com/soccerkick/controller/gBoardController.java
+++ myProject/src/main/java/com/soccerkick/controller/gBoardController.java
@@ -1,10 +1,8 @@
 package com.soccerkick.controller;
 
-import java.io.IOException;  
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Calendar;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.mybatis.spring.SqlSessionTemplate;
@@ -15,13 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.soccerkick.dao.FormationDAO;
 import com.soccerkick.dao.GboardDAO;
 import com.soccerkick.dao.MatchingDAO;
 import com.soccerkick.dao.PlaceDAO;
 import com.soccerkick.dao.TeamCreateDAO;
+import com.soccerkick.dao.TeamDAO;
+import com.soccerkick.vo.Formation_41212VO;
+import com.soccerkick.vo.Formation_4231VO;
+import com.soccerkick.vo.Formation_433VO;
+import com.soccerkick.vo.GameBoardVO;
 import com.soccerkick.vo.GboardVO;
 import com.soccerkick.vo.MatchingVO;
-import com.soccerkick.vo.MemberSelectVO;
 import com.soccerkick.vo.PlaceVO;
 import com.soccerkick.vo.TeamVO;
 import com.soccerkick.vo.userVO;
@@ -49,9 +52,21 @@ public class gBoardController{
 		return "redirect:/";
 	}
 	
-	@RequestMapping("/gameList")
-	public String team_open(){
-		
+	@RequestMapping(value = "/gameList", method = RequestMethod.GET)
+	public ModelAndView home(Model model) {
+
+		ModelAndView mv = new ModelAndView();
+		GboardDAO dao = sqlSession.getMapper(GboardDAO.class);
+		ArrayList<GameBoardVO> list = dao.execGameList();
+		mv.addObject("list", list);
+		mv.setViewName("/gBoard/gameList");
+
+		return mv;
+	}
+	
+	@RequestMapping("/team_open")
+	public String team_open() {
+
 		return "/gBoard/team_open";
 	}
 	
@@ -127,7 +142,43 @@ public class gBoardController{
 		return mv;
 	}
 
+	@RequestMapping("/gameInfo")
+	public ModelAndView board_content(int team_id, String client_id, int gameno) {
+		ModelAndView mv = new ModelAndView();
+		TeamDAO dao = sqlSession.getMapper(TeamDAO.class);
+		// formation dao
+		FormationDAO fdao = sqlSession.getMapper(FormationDAO.class);
 
+		TeamVO vo = dao.execContent(team_id);
+		GameBoardVO gvo = dao.execGameInfo(client_id, gameno);
+
+		if (vo.getTeam_formation().equals("433")) {
+			System.out.println("433 selected");
+
+			Formation_433VO fvo = fdao.execSelectFormation_433(team_id);
+			mv.addObject("fvo", fvo);
+
+		} else if (vo.getTeam_formation().equals("4231")) {
+			System.out.println("4231 selected");
+
+			Formation_4231VO fvo = fdao.execSelectFormation_4231(team_id);
+			mv.addObject("fvo", fvo);
+		} else if (vo.getTeam_formation().equals("41212")) {
+			System.out.println("41212 selected");
+
+			Formation_41212VO fvo = fdao.execSelectFormation_41212(team_id);
+			mv.addObject("fvo", fvo);
+		}
+
+		// get team position by team_id
+
+		// get team players
+		mv.addObject("vo", vo);
+		mv.addObject("gvo", gvo);
+		mv.setViewName("/gBoard/gameInfo");
+
+		return mv;
+	}
 	
 	
 }
